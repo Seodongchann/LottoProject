c91de9c3e004200f30c7f89000f2123138194006
SeoDongChann <anoloveme@naver.com>
2024-08-05 09:58:34 +0900

    Seo - 구매 장수를 입력하는 대화상자에서 프로그램 종료수정

--- LottoProject/src/kr/co/lotto/LottoMain.java
+++ LottoProject/src/kr/co/lotto/LottoMain.java
@@ -1,6 +1,7 @@
 package kr.co.lotto;
 
-import java.awt.Component;
+import java.awt.Frame;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,26 +15,33 @@ public class LottoMain extends JPanel {
 	Random r = new Random();
 
 	public LottoMain() {
-
+		JFrame frame = new JFrame();
 		LottoNum.s = new ArrayList<LottoStart>();
 		while (true) {
 			try {
 				String s = JOptionPane.showInputDialog("몇장을 사시겠습니까?");
-				int num = Integer.parseInt(s);
-				LottoNum.count = num;
-				for (int i = 0; i < num; i++) {
-					LottoNum.s.add(new LottoStart(i));
-					LottoNum.s.get(i).setVisible(true);
-					LottoNum.listM.add(new HashMap<String, List<Integer>>());
-					LottoNum.LIST2.add(new ArrayList<String>());
+				if (!s.equals(null)) {
+					int num = Integer.parseInt(s);
+					LottoNum.count = num;
+					for (int i = 0; i < num; i++) {
+						LottoNum.s.add(new LottoStart(i));
+						LottoNum.s.get(i).setVisible(true);
+						LottoNum.listM.add(new HashMap<String, List<Integer>>());
+						LottoNum.LIST2.add(new ArrayList<String>());
+					}
+				} else if (s.equals(null)) {
+					System.exit(0);
+
 				}
 				당첨번호추첨메소드(r);
 				break;
+			} catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(frame, "숫자를 입력해 주세요!!!", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "메롱", "ERROR_MESSAGE", JOptionPane.ERROR_MESSAGE);
+				break;
 			}
-		}
 
+		}
 	}
 
 	public static void main(String[] args) {
